8dcdc01555451c8308d8d7a8c47b152ab0ca0c6e
Manuel Mauro <manuel.mauro@protonmail.com>
2024-11-10 13:56:53 +0100

    feat: add FiraCode font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'fira': ['"Fira Code"', 'monospace'],
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
